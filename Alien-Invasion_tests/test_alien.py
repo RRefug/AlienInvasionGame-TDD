cd50e11c6cacbfac134c5ad02c1ab35657f75e13
RRefug <99227044+RRefug@users.noreply.github.com>
2025-08-03 10:47:44 -0700

    Change variable name in test_alien

--- Alien-Invasion_tests/test_alien.py
+++ Alien-Invasion_tests/test_alien.py
@@ -12,11 +12,11 @@ import pytest
 @pytest.fixture
 def game_screen_display():
     pygame.init() # making sure Pygame is initialized safely
-    screen_width = 200
-    screen_height = 200
-    screen_size = (screen_width, screen_height)
-    screen = pygame.display.set_mode(screen_size)
-    yield screen   # returns screen
+    game_screen_width = 200
+    game_screen_height = 200
+    game_screen_size = (game_screen_width, game_screen_height)
+    game_screen = pygame.display.set_mode(game_screen_size)
+    yield game_screen   # returns screen
     pygame.quit()   # closes screen too
 
 @pytest.fixture
